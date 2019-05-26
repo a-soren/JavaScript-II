117f32af5d47d42fb979f62b342adeca5af750d1
Amber Sorensen <ambersorensen@Ambers-MacBook-Pro.local>
2019-05-26 16:23:49 -0700

    finished array method assignment creating and solving problems using forEach map and filter

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -81,19 +81,55 @@ for (let i=0;i<runners.length;i++){
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
-console.log(largeShirts);
+largeShirts=runners.filter((size)=>{
+    return(size.shirt_size==='L');
+  
+  });
+  console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
-console.log(ticketPriceTotal);
+ticketPriceTotal=runners.reduce((total, donationAmount)=>{
+    return total+=donationAmount.donation;
+  },0);
+  
+  console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
+// The bib printer wont print bibs in all caps, we need to return back to lower case. (.map)
+let lowerCase=[];
+
+for (let i=0;i<runners.length;i++){
+  let lower=runners[i].first_name;
+  lowerCase.push(runners[i].first_name)
+  
+}
+console.log(lowerCase);
+lowerCase.map(function lower(item){
+  return item.toLowerCase();
+});
 
 // Problem 2
+// we need to get all the even number runners to send a different start time (.filter)
+
+evenId=runners.filter(number=>{
+    return number.id %2===0;
+  });
+  
+  console.log(evenId);
 
 // Problem 3
+// we need a list of each company and their donation amount to send a receipt for write off's. (.forEach)
+let receipt=[];
+
+  for (let i=0;i<runners.length;i++){
+    let taxWriteOff=`${runners[i].company_name} amount: $${runners[i].donation}`;
+    receipt.push(taxWriteOff)
+  }
+
+runners.forEach((cName,index)=>{
+  console.log(receipt);
+ });
